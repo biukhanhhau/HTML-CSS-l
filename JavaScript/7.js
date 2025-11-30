1f01398745b7ddca6629b23cc514a55228606d34
biukhanhhau <biu12.02dev@gmail.com>
2025-11-30 01:42:24 -0500

    learn

---
+++ JavaScript/7.js
@@ -0,0 +1,24 @@
+console.log(Math.PI);
+console.log(Math.E);
+
+let x = 3.123;
+let z = Math.round(x);   // 3
+console.log(z);
+
+let u = Math.round(3.6);   // 4
+console.log(u);     
+
+console.log(Math.ceil(3.9));   // round up
+
+console.log(Math.floor(3.9));  //round down
+
+console.log(Math.pow(x, z));
+
+console.log(Math.log(z));
+
+console.log(Math.sin(x));    // sin cos tan cot
+
+console.log(Math.sign(x));      // -1 0 -1
+
+console.log(Math.max(x, z ,u));
+console.log(Math.min(x, z ,u));
