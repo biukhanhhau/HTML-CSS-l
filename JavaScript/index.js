9b196763c806b4ee5b10c1e5025f7fa7d1c6d4ae
biukhanhhau <biu12.02dev@gmail.com>
2025-11-29 23:44:30 -0500

    first-js

---
+++ JavaScript/index.js
@@ -0,0 +1,24 @@
+// console.log("hello")
+
+// window.alert("I like burger")
+
+document.getElementById("h1-first").textContent = `I like burgur`;
+
+let age = 100;
+let name = `Thach`;
+let favoriteFood = `pizza`;
+console.log(age + name);
+
+console.log(`your name is ${name}`);
+console.log(`you are ${age} years old`)
+console.log(typeof  age);
+console.log(typeof name);
+console.log(`i like ${favoriteFood}`);
+
+let online = true;
+console.log(online);
+console.log(typeof online);
+console.log(online + age);      //true = 1     false = 0
+
+document.getElementById("first").textContent = `My name is ${name}`;
+document.getElementById("second").textContent = `I'm ${age} years old XD`;
