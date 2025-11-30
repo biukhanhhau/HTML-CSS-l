75f685dad5ab45dc583fa75703382f9bc4fc7f0e
biukhanhhau <biu12.02dev@gmail.com>
2025-11-30 03:26:51 -0500

    update Switch

--- JavaScript/12.js
+++ JavaScript/12.js
@@ -23,3 +23,24 @@ switch(day){
         console.log("It is Sunday");
         break;
 }
+
+
+let testScore = 75;
+let letterGrade;
+
+switch(true){
+    case testScore >= 90:
+        letterGrade = `A`;
+        break;
+    case testScore >= 80:
+        letterGrade = `B`;
+        break;
+    case testScore >= 70:
+        letterGrade = `C`;
+        break;
+    case testScore >= 60:
+        letterGrade = `D`;
+        break;
+}
+
+console.log(letterGrade);
