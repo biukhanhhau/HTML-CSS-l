c7cbdc0a1978ee14c5dbb0f18c50b5c1c5b36452
biukhanhhau <biu12.02dev@gmail.com>
2025-11-29 23:51:55 -0500

    operator

---
+++ JavaScript/2.js
@@ -0,0 +1,22 @@
+let student = 30;
+
+// student = student - 5;
+// student = student + 5;
+// student = student * 5;
+// student = student / 5;
+// student = student ** 5;
+// student = student % 2;
+
+
+// student += 5;
+// student -= 5;
+// student *= 5;
+// student /= 5;
+// student **= 5;
+// student %= 5;
+
+
+student --;
+student ++;
+
+console.log(student);
